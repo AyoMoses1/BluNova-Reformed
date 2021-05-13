4d9b5b0522f1320989f961e4177fe4bbf4a59a57
Ayo Moses <ayocandy1@gmail.com>
2021-05-13 10:52:25 -0700

    Second Attempt

---
+++ index.js
@@ -0,0 +1,28 @@
+// $('.carousel-container').slick({
+//   slidesToShow: 3,
+//   slidesToScroll: 1,
+//   autoplay: true,
+//   autoplaySpeed: 2000,
+// })
+$('.carousel-container').slick({
+    autoplay:true,
+    autoplaySpeed:3000,
+    nextArrow:$('.next'),
+    prevArrow:$('.prev'),
+})
+$('.banner').slick({
+    autoplay:true,
+    autoplaySpeed:4000,
+    nextArrow:$('.hero-next'),
+    prevArrow:$('.hero-prev'),
+})
+var height = $('header').height();
+
+$(window).scroll(function(){
+    if($(this).scrollTop() > height ){
+        $('.navbar').addClass('fixed')
+    }
+    else{
+        $('.navbar').removeClass('fixed')
+    }
+});
