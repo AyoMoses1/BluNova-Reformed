68faa1997fb6179398fe4918aa039138068be9b6
Ayo Moses <ayocandy1@gmail.com>
2021-05-17 23:18:42 -0700

    responsive design

--- index.js
+++ index.js
@@ -27,4 +27,11 @@ $(window).scroll(function(){
     }
 });
 
- 
+// Hamburger Menu
+const toggleButton  = document.getElementsByClassName("toggle-button")[0];
+
+const navLinks = document.getElementsByClassName("nav-links")[0];
+
+ toggleButton.addEventListener('click', ()=>{
+     navLinks.classList.toggle('active');
+ })
